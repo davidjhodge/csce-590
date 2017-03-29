298cfd2d0c3bbf01f12e944ee6ecba56cc7fef88
davidjhodge <dhodge@genesisappsonline.com>
2017-03-29 09:24:31 -0400

    HW 4 Support Photo Upload and Viewing for Orders
    Each order can now have a photo associated with it. In addition,
    images show throughout the app.

---
+++ HW4/orderApp/www/js/app.js
@@ -0,0 +1,98 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'ui.router', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+
+    appid = 'EXWkCHVJeyBIUuKU1BPPXhTTQZPHIRLLFcdITXgs';
+    jskey = 'OMVXSdqgaWXXMI7q1rt1K5LLoQQs5FCYdjoOr8jA';
+    Parse.initialize(appid, jskey);
+    Parse.serverURL = 'https://parseapi.back4app.com';
+    console.log("Connecting to database...");
+
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // Login state
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+
+  // Create Account state
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'LoginCtrl'
+  })
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  // Orders
+  .state('tab.orders', {
+    url: '/orders',
+    views: {
+      'tab-orders': {
+        templateUrl: 'templates/tab-orders.html',
+        controller: 'OrdersCtrl'
+      }
+    }
+  })
+
+  // Order detail
+  .state('order-detail', {
+    url: '/tab/orders/:orderId',
+    params: {
+      orderId: null
+    },
+    templateUrl: 'templates/order-detail.html',
+    controller: 'OrderDetailCtrl'
+  })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
---
+++ HW4/orderApp/www/js/controllers.js
@@ -0,0 +1,230 @@
+// var Parse = require('parse');
+
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $ionicModal, $state) {
+  var self = this;
+
+  // Routing
+  $scope.showSignup = function() {
+    $state.go('signup');
+  }
+
+  $scope.showLogin = function() {
+    $state.go('login');
+  }
+
+  // Authentication
+  $scope.signIn = function(user) {
+    var username = user.email;
+    var password = user.password;
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+
+    if (username && username.length > 0 && password && password.length > 0) {
+
+      Parse.User.logIn(username, password, {
+        success: function(user) {
+          // Login Success!
+          console.log(user.id);
+
+          $state.go("tab.orders");
+        },
+        error:function(user,error) {
+          alert("Login Error: " + error.message);
+        }
+      });
+    }
+  }
+
+  $scope.createAccount = function(user) {
+    // Print input
+    var username = user.email;
+    var password = user.password;
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+
+    if (username && username.length > 0 && password && password.length > 0) {
+
+      var user = new Parse.User();
+      user.set("username", username);
+      user.set("password", password);
+      user.set("email", username);
+
+      user.signUp(null, {
+        success: function(user) {
+          // Successfully created user
+          console.log(user.id);
+
+          $state.go("tab.orders");
+        },
+        error: function(error) {
+          console.log(error.message);
+          alert("Sign Up Error: " + error.message);
+        }
+      });
+    }
+  }
+})
+
+.controller('DashCtrl', function($scope) {})
+
+// Orders controller
+.controller('OrdersCtrl', function($scope, $ionicModal, $state) {
+  $scope.getOrders = function() {
+    var Order = Parse.Object.extend("Order");
+    var query = new Parse.Query(Order);
+    query.find({
+      success: function(orders) {
+        console.log("Successfully retrieved " + orders.length + " orders.");
+        // console.log(JSON.stringify(orders));
+
+        $scope.orders = [];
+        for (var i = orders.length - 1; i >= 0; i--) {
+
+          $scope.orders.push({
+            id: orders[i].id,
+            productQuantity: orders[i].get("productQuantity"),
+            productName: orders[i].get("productName"),
+            productPrice: orders[i].get("productPrice"),
+            productCategory: orders[i].get("productCategory"),
+            orderDate: orders[i].get("orderDate").toString(),
+            productImage: orders[i].get("productImage")
+          });
+        }
+
+        $scope.$apply();
+      }
+    });
+  }
+  // Initially get the orders when controller loads
+  $scope.getOrders();
+
+  $scope.viewOrder = function(orderId) {
+    $state.go('order-detail', {
+      orderId: orderId
+    });
+  }
+
+  // Create Order Modal
+  $ionicModal.fromTemplateUrl('templates/create-order.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.createNewOrder = function() {
+    $scope.modal.show();
+  }
+
+  $scope.cancelNewOrder = function() {
+    $scope.modal.hide();
+  }
+
+  $scope.submitOrder = function(product) {
+    var productName = product.name;
+    var quantity = product.quantity;
+    var price = product.price;
+    var category = product.category;
+    var date = product.orderDate;
+
+    var imageInput = document.getElementById("add-picture");
+    if (imageInput.files.length === 0) {
+      console.log("Image is empty.");
+      return;
+    }
+    var imageFile = imageInput.files[0];
+    var imageName = "orderImage.jpg";
+    var parseFile = new Parse.File(imageName, imageFile);
+
+    // Save Image file first
+    parseFile.save().then(() => {
+      // The file has been saved to Parse.
+
+      // Create new Order object
+      var Order = Parse.Object.extend("Order");
+      var m = new Order();
+
+      // User who placed the order
+      m.set("orderedBy", Parse.User.current());
+      m.set("productName", productName);
+      m.set("productQuantity", quantity);
+      m.set("productPrice", price);
+      m.set("productCategory", category);
+      m.set("orderDate", date);
+      // Order date is the createdAt field
+      // objectId is created by default
+      m.set("productImage", parseFile);
+
+      m.save().then((o) => {
+        // Refresh Orders
+        $scope.getOrders();
+
+        $scope.modal.hide();
+      });
+
+    }, (error) => {
+      // The file either could not be read, or could not be saved to Parse.
+      console.log(error);
+    });
+  }
+
+  $scope.remove = function(order) {
+    var Order = Parse.Object.extend("Order");
+    var query = new Parse.Query(Order);
+    query.get(order.id, {
+      success: function(storedOrder) {
+        // The object was retrieved successfully.
+        storedOrder.destroy({});
+        $scope.getOrders();
+        $scope.$apply();
+      },
+      error: function(object, error) {
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and description.
+        alert("Remove Error: " + error.message);
+      }
+    });
+  }
+})
+
+.controller('OrderDetailCtrl', function($scope, $ionicHistory, $state) {
+  const orderId = $state.params.orderId;
+
+  var Order = Parse.Object.extend("Order");
+  var query = new Parse.Query(Order);
+  query.get(orderId, {
+    success: function(storedOrder) {
+      // The object was retrieved successfully.
+
+      $scope.order = {
+        id: storedOrder.id,
+        productQuantity: storedOrder.get("productQuantity"),
+        productName: storedOrder.get("productName"),
+        productPrice: storedOrder.get("productPrice"),
+        productCategory: storedOrder.get("productCategory"),
+        orderDate: storedOrder.get("orderDate").toString(),
+        productImage: storedOrder.get("productImage")
+      };
+      $scope.$apply();
+      console.log("Located order " + storedOrder.id) + ".";
+    },
+    error: function(object, error) {
+      // The object was not retrieved successfully.
+      // error is a Parse.Error with an error code and description.
+      alert("Get Order Error: " + error.message);
+    }
+  });
+
+  $scope.goBack = function() {
+    console.log($ionicHistory.viewHistory());
+    $ionicHistory.goBack();
+  }
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
